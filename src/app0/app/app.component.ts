c2b0868c6672234f6317b887c9ec279aed4ee4bf
mk-84 mint <manuel_kraemer@gmx.net>
2018-02-12 20:35:00 +0100

    -apiUrl\n-copyright

--- src/app0/app/app.component.ts
+++ src/app0/app/app.component.ts
@@ -11,12 +11,16 @@ import { Observable } from 'rxjs/Observable';
 export class AppComponent
 {
     currentUser: Observable<LoginUser>;
-    year: number;
+    year: string;
     daysLeftToChristmas: number;
 
     constructor(private storageHandler: StorageHandler, private alertService: AlertService) {
         this.currentUser = this.storageHandler.currentUser;
-        this.year = new Date().getFullYear();
-        this.daysLeftToChristmas = Math.ceil((new Date(this.year + "-12-24 00:00:00").valueOf() - new Date().valueOf()) / 10000 / 360 / 24)
+        var yearInt  = new Date().getFullYear();
+        if(yearInt <= 2017)
+            this.year = "2017";
+        else
+            this.year = "2017 - " + yearInt;
+        this.daysLeftToChristmas = Math.ceil((new Date(yearInt + "-12-24 00:00:00").valueOf() - new Date().valueOf()) / 10000 / 360 / 24)
     }
 }
--- src/app0/environments/environment.ts
+++ src/app0/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/restservice/'
+  apiUrl: 'http://localhost/php/restservice/'
 };
